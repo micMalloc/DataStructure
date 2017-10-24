04881ad56828ce69e18e988739573cd429f1900e
micMalloc <hesue615@naver.com>
2017-10-24 14:43:08 +0900

    Adjacency Matrix
    Representation of Graphs

---
+++ Graphs/Representation/AdjacencyMatrix.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#define MAX 100
+
+int adjacentMatrix[MAX][MAX];
+
+int main (void) {
+	int n;
+	int u, v;
+	int i;
+
+	// N is the number of the edges of a Graph
+	// U and V are nodes which are adjacent each other
+
+	scanf("%d", &n);
+	for (i = 0; i < n; ++ i) {
+	 // Consturct a Graph from given edge's information
+	 // Format : /'U V/' meaning U and V are Adjacent
+		scanf("%d %d", &u, &v);
+
+	 // 1 is meaning that the two nodes are adjacent
+		// If a edge is directed edge
+		adjacentMatrix[u][v] = 1;
+
+		// If a edge is undirected edge
+		adjacentMatrix[u][v] = 1;
+		adjacentMatrix[v][u] = 1;
+	}
+
+	return 0;
+}
