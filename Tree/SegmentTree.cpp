dd3e30c2827947f8deaaeb2b7ee5a629ac10be01
micMalloc <hesue615@naver.com>
2018-02-15 17:35:57 +0900

    Add update_segment_tree

--- Tree/SegmentTree.cpp
+++ Tree/SegmentTree.cpp
@@ -1,38 +1,44 @@
 #include <cstdio>
+#include <cmath>
 #include <vector>
 using namespace std;
 
 const int MAX = 10;
 
-int makeSegmentTree (int, int, int);
-int get_solution (int, int, int, int);
-void update_segTree ();
+int init_segment_tree (int, int, int);
+int get_solution (int, int, int, int, int);
+void update_segment_tree (int, int, int, int, int);
 
 int data[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-vector<int> segTree;
+vector<int> segment_tree;
 
 int main () {
 	int n;
+	int h, tree_size;
 
 	scanf("%d", &n);
-	segTree.resize(4 * n);
-	segTree[0] = 0;
+
+	h = (int)ceil(log(n) / log(2));
+	tree_size = (1 << (h + 1));
+
+	segment_tree.resize(tree_size);
+	segment_tree[0] = 0;
 	
-	makeSegmentTree(1, 0, n - 1);
+	init_segment_tree(1, 0, n - 1);
 
 	return 0;
 }
 /* Get Sum of Given Range */
-int makeSegmentTree (int loca, int start, int end) {
+int init_segment_tree (int loca, int start, int end) {
 	int mid;
 
 	if (start < end) {
 		mid = (start + end) / 2;
-		segTree[loca] += makeSegmentTree(2 * loca, start, mid);
-		segTree[loca] += makeSegmentTree((2 * loca) + 1, mid + 1, end);
+		segment_tree[loca] += init_segment_tree(2 * loca, start, mid);
+		segment_tree[loca] += init_segment_tree((2 * loca) + 1, mid + 1, end);
 	} else {
-		segTree[loca] = data[start];
-	} return segTree[loca];
+		segment_tree[loca] = data[start];
+	} return segment_tree[loca];
 }
 
 int get_solution (int curr, int start, int end, int left, int right) {
@@ -43,7 +49,7 @@ int get_solution (int curr, int start, int end, int left, int right) {
 		return 0;
 	}
 	if (left <= start || end <= right) {
-		return segTree[curr];
+		return segment_tree[curr];
 	}
 
 	mid = (start + end) / 2;
@@ -53,6 +59,15 @@ int get_solution (int curr, int start, int end, int left, int right) {
 	return ans;
 }
 
-void update_segTree () {
-	
+void update_segment_tree (int node, int start, int end, int idx, int dif) {
+	int mid;
+
+	if (idx < start || idx > end) return;
+
+	segment_tree[node] += dif;
+	if (start != end) {
+		mid = (start + end) / 2;
+		update_segment_tree(node * 2, start, mid, idx, dif);
+		update_segment_tree(node * 2 + 1, mid + 1, end, idx, dif);
+	}
 }
